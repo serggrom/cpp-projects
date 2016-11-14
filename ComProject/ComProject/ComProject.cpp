78c684a126414565d8d1f21bf31224106cefc832
sergg_grom <sergg.grom@gmail.com>
2016-11-14 11:44:33 +0300

    first Com port project

---
+++ ComProject/ComProject/ComProject.cpp
@@ -0,0 +1,14 @@
+// ComProject.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "../../std_lib_facilities.h"
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello world!";
+	return 0;
+}
+
