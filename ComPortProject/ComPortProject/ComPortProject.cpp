506bc6e1f3adbafa177f7e71bcb62460b36ab670
sergg_grom <sergg.grom@gmail.com>
2016-11-14 15:39:31 +0300

    add hello word

---
+++ ComPortProject/ComPortProject/ComPortProject.cpp
@@ -0,0 +1,14 @@
+// ComPortProject.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "../../std_lib_facilities.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+
+{
+	cout << "Hello!\n";
+	return 0;
+}
+
