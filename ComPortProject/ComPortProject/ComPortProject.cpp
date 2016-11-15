7503e39dbf5122b342ac608157b0b210e4a00f29
sergg_grom <sergg.grom@gmail.com>
2016-11-15 14:20:23 +0300

    add finding of ports in system

--- ComPortProject/ComPortProject/ComPortProject.cpp
+++ ComPortProject/ComPortProject/ComPortProject.cpp
@@ -4,13 +4,42 @@
 #include "stdafx.h"
 #include "../../std_lib_facilities.h"
 #include "SerialGate.h"
+#include <conio.h>
+#include <windows.h>
 
 
 int _tmain(int argc, _TCHAR* argv[])
 
 {
 	cout << "Hello!\n";
+
+	cout << "Detect all COM ports in system\n";
+
+	SerialGate sg;
+
+	PortInfo pi;
+
+	sg.GetPortsInfo(&pi);
+
+	printf("Total COM ports: %d\n\n", pi.koll);
+	//cout << "Total COM ports: %d" << pi.koll << "\n";
+
+	for (int i = 0; i < pi.koll; i++)
+	{
+		if (pi.p[i].Availbl == true)
+		{
+			printf("COM%d - free\n", pi.p[i].Id);
+			//cout << "COM" << pi.p[i].Id << " - free\n";
+		}
+		else 
+		{
+			printf("COM%d - inaccessible\n", pi.p[i].Id);
+			//cout << "COM" << pi.p[i].Id << " - inaccessible\n";
+		}
+	}
+
 	cout << "Choose ports\n";
-	return 0;
+
+	_getch();
 }
 
