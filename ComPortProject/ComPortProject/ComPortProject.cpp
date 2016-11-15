1ed04c2f3c21f6d347076604936960618a76d41f
sergg_grom <sergg.grom@gmail.com>
2016-11-15 15:08:10 +0300

    start working with COM port

--- ComPortProject/ComPortProject/ComPortProject.cpp
+++ ComPortProject/ComPortProject/ComPortProject.cpp
@@ -7,6 +7,10 @@
 #include <conio.h>
 #include <windows.h>
 
+int UserChoosePort = 0;
+int dwBytesRead = 0;
+
+
 
 int _tmain(int argc, _TCHAR* argv[])
 
@@ -38,8 +42,70 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 	}
 
-	cout << "Choose ports\n";
+	cout << "Choose port from free ports\n";
+	cout << "Please, input port number like 1 or any number.\n";
+	cin >> UserChoosePort;
+	int rate = 9600;
+	bool Port = sg.Open(UserChoosePort, rate);
+	if (Port == false)
+	{
+		cout << "Open Error.\n";
+	}
+	else
+	{
+		cout << "Port open successfully\n";
+	}
 
+	cout << "Press key to send command to COM port\n";
+	cout << "Press '#' to exit\n";
+
+	while (true)
+	{
+		char c = _getch();
+		printf("%c", c);
+
+		sg.Send(&c, sizeof(c));
+
+		if (c == '#')
+		{
+			break;
+		}
+
+		cout << "\nGet data from COM port every 0.01 sec.\n";
+
+		bool terminate = false;
+		char ReadBuffer[20];
+
+
+		while(!terminate)
+		{
+			Sleep(10);
+			dwBytesRead = sg.Recv(ReadBuffer, sizeof(ReadBuffer));
+
+			for (int i = 0; i < dwBytesRead; i++)
+			{
+				printf("%c", ReadBuffer[i]);
+				if (ReadBuffer[i] == '#')
+				{
+					terminate = true;
+					break;
+				}
+			}
+		}
+
+
+
+
+		
+	}
+
+
+
+
+
+
+
+	
 	_getch();
 }
 
