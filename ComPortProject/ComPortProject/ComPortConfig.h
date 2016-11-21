e3ab78b4c8507a86d6190112c37f2c681265a471
sergg_grom <sergg.grom@gmail.com>
2016-11-21 11:56:16 +0300

    create com port configuration header

--- ComPortProject/ComPortProject/ComPortConfig.h
+++ ComPortProject/ComPortProject/ComPortConfig.h
@@ -1,3 +1,38 @@
-#include <windows.h>
-#include <iostream>
-#include <assert.h>
+#ifndef TTY_H
+#define TTY_H
+
+#define  NOMINMAX
+
+#include <Windows.h>
+
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+struct TTY 
+{
+
+	TTY();
+	virtual ~TTY();
+
+	bool IsOK() const;
+
+	void Connect (const string& port, int baudrate);
+	void Disconnect();
+
+	virtual void Write (const vector <unsigned char> & data);
+	virtual void Read (vector <unsigned char> & data);
+
+	HANDLE m_Handle;
+
+};
+
+
+struct TTYException
+{
+};
+
+
+#endif;
