1abd36051a5d135541635d0cb538bf01af6f2007
sergg_grom <sergg.grom@gmail.com>
2017-02-02 14:25:47 +0300

    add reading string line with space

--- english_grammar/english_grammar/main.cpp
+++ english_grammar/english_grammar/main.cpp
@@ -1,5 +1,9 @@
 #include "../../std_lib_facilities.h"
+//#include <sstream>
 
+using namespace std;
+
+/*
 int method1 (int a, int n)
 {
 	int p = 0;
@@ -35,13 +39,15 @@ int method2 (int a, int n)
 	}
 	return p;
 }
+*/
 
 
 int main()
 {
-	//String InputSentence = "";
-	//cout << "Please, input the sentence in English\n";
-	//cin >> InputSentence;
+	string InputSentence = " ";
+	cout << "Please, input the sentence in English\n";
+	getline(cin, InputSentence);
+	/*
 	int A, N;
 	cout << "Input 2 integers\n";
 	cin >> A >> N;
@@ -49,4 +55,30 @@ int main()
 	int answ2 = method2(A, N);
 	cout << "Result: " << answ << "\n";
 	cout << "Result: " << answ2 << "\n";
+	*/
+	int n = InputSentence.length();
+	cout << "Size of inputed string: " << n << "\n";
+	cout << InputSentence << "\n";
+	char n3 = InputSentence[n-3];
+	char n2 = InputSentence[n-2];
+	char n1 = InputSentence[n-1];
+	if ( n3 == ' ' && n2 == '.'
+		&& n1 == ' ')
+	{
+		cout << "This is a correct end of the sentence!\n" ;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
