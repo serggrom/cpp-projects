d4a5f7e74ecb0af913ef1057ec8d4b87feb48a72
sergg_grom <sergg.grom@gmail.com>
2017-02-08 17:15:25 +0300

    chapter 8 drill

---
+++ Part8/Part8/use.cpp
@@ -0,0 +1,17 @@
+#include "my.h"
+#include "../../std_lib_facilities.h"
+#include <iostream>
+
+//int foo;
+
+
+/*
+int main()
+{
+	//char cc;
+	//std::cin >> cc;
+	foo = 7;
+	print_foo();
+	print(99);
+}
+*/
