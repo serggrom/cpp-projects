cc19d924247b89fa244ac2f3d8d593be38355d34
sergg_grom <sergg.grom@gmail.com>
2017-02-10 15:08:12 +0300

    end of chapter 8

--- Part8/Part8/main.cpp
+++ Part8/Part8/main.cpp
@@ -127,6 +127,8 @@ void print(vector<int>& v)
 	}
 }
 
+
+
 void print_str(vector<string>& v)
 {
 	for (int i = 0; i < v.size(); ++i)
@@ -194,6 +196,89 @@ void sort_str2(vector<string>& v)
 }
 
 
+double sum_of_multiply(vector<double>& v, vector<double>& w)
+{
+	if (v.size() == w.size())
+	{
+		double sum = 0;
+		for (int i = 0; i < v.size(); ++i)
+		{
+			sum =+ v[i]*w[i];
+		}
+		return sum;
+	}
+	else
+	{
+		error("Vectors must have same size!");
+	}
+}
+
+int maxv (vector<int>& v)
+{
+	int max = v[0];
+	for(int i = 0; i < v.size(); ++i)
+	{
+		if (max < v[i])
+		{
+			max = v[i];
+		}
+	}
+	return max;
+}
+
+void properties_of_vector(vector<double> v)
+{
+	double max = v[0];
+	for(int i = 0; i < v.size(); ++i)
+	{
+		if (max < v[i])
+		{
+			max = v[i];
+		}
+	}
+
+
+	double min = v[0];
+	for(int i = 0; i < v.size(); ++i)
+	{
+		if (v[i] < min)
+		{
+			min = v[i];
+		}
+	}
+
+	double mediana;
+	sort(v.begin(), v.end());
+	if (v.size() % 2 != 0)
+	{
+		int med_count = (v.size()+1) / 2;
+		mediana = v[med_count];
+	}
+	else
+	{
+		int med_count1 = v.size()/2;
+		int med_count2 = (v.size() + 1) /2;
+		mediana = (v[med_count1] + v[med_count2]) / 2;
+	}
+
+	double sum;
+	for (int i = 0; i < v.size(); ++i)
+	{
+		sum =+ v[i];
+	}
+	double medium_value = sum / v.size();
+	
+	cout << "Maximum value: " << &max << "\n";
+	cout << "Minimum value: " << &min << "\n";
+	cout << "Mediana's value: " << &mediana << "\n";
+	cout << "Medium value: " << &medium_value << "\n";
+}
+
+
+
+
+
+
 
 int main()
 {
@@ -258,6 +343,9 @@ int main()
 	
 	vector<string> name_v;
 	vector<int> age_v;
+
+	vector<double> price;
+	vector<double> weight;
 	
 	/*
 	name.push_back("cadfem");
@@ -270,17 +358,26 @@ int main()
 	print_str(name);
 	*/
 
-	for (int i = 0; i < 5; ++i)
+
+	/*
+	while (true)
 	{
 		string name;
 		int age;
-		cin >> name >> age;
+		cin >> name;
+		if (name == "quit")
+		{
+			break;
+		}
+		cin >> age;
 		name_v.push_back(name);
 		age_v.push_back(age);
 	}
 	vector<string> copy_name_v = name_v;
 	sort(name_v.begin(), name_v.end());
-	for (int i = 0; i < name_v.size(); ++i)
+	int med1 = name_v.size()/2;
+	//int med2 = copy_name_v.size()/2;
+	for (int i = 0; i < med1; ++i)
 	{
 		for (int j = 0; j < copy_name_v.size(); ++j)
 		{
@@ -293,4 +390,60 @@ int main()
 
 	print_str(name_v);
 	print(age_v);
+	*/
+	 /*
+	while (true)
+	{
+		double one_price;
+		double one_wight;
+		cin >> one_price;
+		cin >> one_wight;
+		price.push_back(one_price);
+		weight.push_back(one_wight);
+		cout << "Do you want to exit?\n";
+		string exit;
+		cin >> exit;
+		if (exit == "yes")
+		{
+			break;
+		}
+	}
+	cout << sum_of_multiply(price, weight) << "\n";
+	*/
+
+	vector<int> counts;
+	vector<double> statistics;
+	statistics.push_back(12);
+	statistics.push_back(360);
+	statistics.push_back(150);
+	statistics.push_back(8.5);
+	statistics.push_back(22.8);
+	statistics.push_back(3.14);
+
+
+
+
+	/*
+	while (true)
+	{
+		int  count;
+		cin >> count;
+		counts.push_back(count);
+		cout << "Do you want to exit?\n";
+		string exit;
+		cin >> exit;
+		if (exit == "yes")
+		{
+			break;
+		}
+	}
+	*/
+
+	//cout << maxv(counts) << "\n";
+	
+	
+	properties_of_vector(statistics);
+
+
+
 }
